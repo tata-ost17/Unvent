04bba187c2bd840d91f23ef4e7af3004edf4c819
tata-ost17.github.io <63121337+tata-ost17@users.noreply.github.com>
2021-05-29 19:07:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,16 +7,24 @@ $(function () {
       slidesToScroll: 1,
       responsive: [
          {
-            breakpoint: 992,
+            breakpoint: 560,
             settings: {
                slidesToShow: 2,
             }
-         }         
+         },
+         
       ]
-    
-   
    });
 
+   $('.menu__btn').on('click', function () {
+      $('.menu__btn').toggleClass('menu__btn--active');
+      $('.menu__list').toggleClass('menu__list--active');
+   });
+
+   $('.menu__list-link').on('click', function () {
+      $('.menu__btn').removeClass('menu__btn--active');
+      $('.menu__list').removeClass('menu__list--active');
+   });
 
 
 });
